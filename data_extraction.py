134f3dd07e71369ecd22c153946b66e5b341e2eb
Gautam Chaudhary <90352884+Urkatha@users.noreply.github.com>
2023-12-18 21:33:42 +0000

    Create data_extraction.py

---
+++ data_extraction.py
@@ -0,0 +1,11 @@
+# data_extraction.py
+
+class DataExtractor:
+    def extract_data_from_csv(self, file_path):
+        pass
+
+    def extract_data_from_api(self, api_url, api_key):
+        pass
+
+    def extract_data_from_s3(self, bucket_name, object_key):
+        pass
