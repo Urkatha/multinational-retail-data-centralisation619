fc35040b49abc47b00033b71dc420cd930e0c46f
Gautam Chaudhary <90352884+Urkatha@users.noreply.github.com>
2023-12-18 21:35:57 +0000

    Create data_cleaning.py

---
+++ data_cleaning.py
@@ -0,0 +1,32 @@
+# data_cleaning.py
+
+class DataCleaning:
+    def clean_csv_data(self, csv_data):
+        pass
+
+    def clean_api_data(self, api_data):
+        pass
+
+    def clean_s3_data(self, s3_data):
+        pass
+
+    def clean_user_data(self, user_data):
+        pass
+
+    def clean_card_data(self, card_data):
+        pass
+
+    def clean_store_data(self, store_data):
+        pass
+
+    def convert_product_weights(self, products_data):
+        pass
+
+    def clean_products_data(self, products_data):
+        pass
+
+    def clean_orders_data(self, orders_data):
+        pass
+
+    def clean_date_times_data(self, date_times_data):
+        pass
